635b437a19b0cd849b341efc348cc0e695d66e89
Neelesh yadav <mktg.eei@gmai.com>
2023-06-23 15:07:23 +0530

    Changed Hashroute to BrowsrRoute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // bootstarp
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -13,13 +13,14 @@ import "./index.scss";
 // setInterval(() => {
 //   window.location.reload();
 // }, 1000 * 30);
+window.open("...Link", "_self/_blank")
 window.apiURL = "https://api.accesssurveykshan.co.in/api/v1";
 // window.apiURL = "http://localhost:8080/api/v1";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </AppProvider>
 );
